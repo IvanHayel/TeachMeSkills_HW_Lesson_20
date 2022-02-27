74bba1b6cec0755f19ace9ae92c01d7822f547c8
Hayel <ivanforwork@yandex.ru>
2022-02-27 14:12:40 +0300

    Unnecessary files removed.

--- src/main/java/by/teachmeskills/task/util/parser/XmlParser.java
+++
@@ -1,9 +0,0 @@
-package by.teachmeskills.task.util.parser;
-
-import java.nio.file.Path;
-
-public interface XmlParser<T> {
-    boolean jaxbEntityToXml(T entity, Path xmlPath);
-
-    T jaxbEntityFromXml(Path xmlPath);
-}
