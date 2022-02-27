a8ca7cf3de652f9fe53a28c551209cb15e05afa5
Hayel <ivanforwork@yandex.ru>
2022-02-27 06:01:42 +0300

    Runner updated.

--- src/main/java/by/teachmeskills/task/Runner.java
+++ src/main/java/by/teachmeskills/task/Runner.java
@@ -1,70 +1,40 @@
 package by.teachmeskills.task;
 
-import by.teachmeskills.task.service.LocationService;
-import by.teachmeskills.task.service.StudentService;
-import by.teachmeskills.task.service.impl.LocationServiceImpl;
-import by.teachmeskills.task.service.impl.StudentServiceImpl;
-import lombok.extern.java.Log;
+import by.teachmeskills.task.dao.LocationDao;
+import by.teachmeskills.task.dao.StudentDao;
+import by.teachmeskills.task.model.Location;
+import by.teachmeskills.task.model.mysql.MySqlQueries;
+import by.teachmeskills.task.util.manager.MySqlDriverManager;
+import by.teachmeskills.task.util.parser.MySqlQueriesXmlParser;
+import by.teachmeskills.task.util.parser.XmlParser;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
-/**
- * Create a Java application to get a list of all students in a group.
- * Create a table with the cities where the students live.
- * Display information about each student in the group and his hometown.
- * Provide operations for adding and removing cities and students.
- */
-
-@Log
 public class Runner {
-    private static final Path PROPERTY_PATH = Paths.get("src/main/resources/app.properties");
-    private static final String JDBC_CONNECTION_URL = "jdbc:mysql://127.0.0.1:3306/course";
-
-    private static Properties properties = new Properties();
+    private static final Path DATABASE_PROPERTY_PATH = Paths.get("src/main/resources/database.properties");
+    private static final Path MYSQL_QUERIES_PATH = Paths.get("src/main/resources/queries.xml");
+    private static final Properties databaseProperties = new Properties();
 
     static {
-        loadProperties();
+        loadDatabaseProperties();
     }
 
     public static void main(String[] args) {
-        try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-        } catch (SQLException e) {
-            log.warning(e.getMessage());
-        }
-
-        try (Connection connection = DriverManager.getConnection(JDBC_CONNECTION_URL, properties)) {
-            StudentService studentService = new StudentServiceImpl(connection);
-//            Student studentWithThirdId = studentService.getStudentById(3);
-//            System.out.println(studentWithThirdId);
-            /*
-             * StudentService usage here ...
-             */
-
-            LocationService locationService = new LocationServiceImpl(connection);
-//            locationService.addLocation(new Location(3, "Belarus", "Grodno"));
-//            System.out.println(locationService.getLocationList());
-            /*
-             * LocationService usage here ...
-             */
-        } catch (SQLException e) {
-            log.warning(e.getMessage());
-        }
+        @Cleanup MySqlDriverManager driverManager = new MySqlDriverManager(databaseProperties);
+        XmlParser<MySqlQueries> parser = new MySqlQueriesXmlParser();
+        MySqlQueries queries = parser.jaxbEntityFromXml(MYSQL_QUERIES_PATH);
     }
 
-    private static void loadProperties() {
-        try (InputStream propertiesStream = Files.newInputStream(PROPERTY_PATH)) {
-            properties.load(propertiesStream);
-        } catch (IOException e) {
-            log.warning(e.getMessage());
-        }
+    @SneakyThrows(IOException.class)
+    private static void loadDatabaseProperties() {
+        @Cleanup InputStream propertiesStream = Files.newInputStream(DATABASE_PROPERTY_PATH);
+        databaseProperties.load(propertiesStream);
     }
 }
